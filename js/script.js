56ce923a0b87817066378a07e8856599a14717ab
vrk-cloud-academy <49936518+vrk-cloud-academy@users.noreply.github.com>
2026-03-29 18:14:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+function loadComponent(id, file) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
 
+loadComponent("header", "components/header.html");
+loadComponent("footer", "components/footer.html");
